5dda735ae320b7ff4b1aa4aee0abd0b414c6ea60
kc07wlaf <kc.goal@gmail.com>
2018-01-19 23:13:15 +0800

    v.0

---
+++ app/src/main/java/com/example/android/draft_vitalsignrecords/EnterNewRecordActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.draft_vitalsignrecords;
+
+/**
+ * Created by kcc on 1/19/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EnterNewRecordActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_new_record);
+    }
+
+
+}
